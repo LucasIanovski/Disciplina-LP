e486b6d7b6ac35e71d6f4aaabfbe2698147ef120
LucasIanovski <ianovskilucas@gmail.com>
2025-04-11 16:27:07 -0300

    Aula 05 Lista 04 Resolução Exercios 1 a 4.

--- Listas de Exercícios/LP_Lista_04/Ex03_L4/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/Ex03_L4/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 200; i++) {
+            if (i % 4 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_04/Ex04_L4/src/Main.java
+++ Listas de Exercícios/LP_Lista_04/Ex04_L4/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int c;
+        double f;
+
+        for (c = -80; c <= 80; c += 10) {
+            f = (9.0 / 5) * c + 32;
+            System.out.println(c + "°C = " + f + "°F");
+        }
     }
 }
