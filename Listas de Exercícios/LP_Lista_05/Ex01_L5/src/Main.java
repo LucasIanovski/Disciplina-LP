45c8ac2d8c425449e8fd53df9f5e81643166308e
LucasIanovski <ianovskilucas@gmail.com>
2025-05-06 16:42:57 -0300

    Aula 06 lista 05 Resolução Exercicios 1 e 2

--- Listas de Exercícios/LP_Lista_05/Ex01_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex01_L5/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int i;
+        for (i = v.length - 1; i >= 0; i--) {
+            System.out.println(v[i]);
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_05/Ex02_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex02_L5/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int i;
+        for (i = 0; i < v.length; i++) {
+            if (i % 2 == 0) {
+                v[i] += 2;
+            } else {
+                v[i] *= 2;
+            }
+            System.out.println(v[i]);
+        }
     }
 }
