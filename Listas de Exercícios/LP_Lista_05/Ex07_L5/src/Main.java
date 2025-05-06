90790ad4632a651c2501d6512e825965bd82a61f
LucasIanovski <ianovskilucas@gmail.com>
2025-05-06 17:20:24 -0300

    Aula 06 lista 05 Resolução Exercicios 1 a 10

--- Listas de Exercícios/LP_Lista_05/Ex07_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex07_L5/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[][] m = new int[5][5];
+        int i, j, soma, maiorLinha = Integer.MIN_VALUE, maiorColuna = Integer.MIN_VALUE;
+        Scanner in = new Scanner(System.in);
+
+        for (i = 0; i < 5; i++) {
+            for (j = 0; j < 5; j++) {
+                System.out.print("m[" + i + "][" + j + "] = ");
+                m[i][j] = in.nextInt();
+            }
+        }
+
+        for (i = 0; i < 5; i++) {
+            soma = 0;
+            for (j = 0; j < 5; j++) {
+                soma += m[i][j];
+            }
+            if (soma > maiorLinha) maiorLinha = soma;
+        }
+
+        for (j = 0; j < 5; j++) {
+            soma = 0;
+            for (i = 0; i < 5; i++) {
+                soma += m[i][j];
+            }
+            if (soma > maiorColuna) maiorColuna = soma;
+        }
+
+        System.out.println("Maior soma das linhas: " + maiorLinha);
+        System.out.println("Maior soma das colunas: " + maiorColuna);
     }
 }
--- Listas de Exercícios/LP_Lista_05/Ex10_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex10_L5/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[][] a = {{1, 2}, {3, 4}, {5, 6}}, b = {{7, 8}, {9, 10}}, c = new int[3][2];
+        int i, j, k;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 2; j++) {
+                c[i][j] = 0;
+                for (k = 0; k < 2; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
