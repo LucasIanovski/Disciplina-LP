e5e5bcc2632ca8c76d03b31d1ba15dc84a43596c
LucasIanovski <ianovskilucas@gmail.com>
2025-05-06 21:02:21 -0300

    Aula 06 lista 05 Resolução Exercicios 1 a 12

--- Listas de Exercícios/LP_Lista_05/Ex11_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex11_L5/src/Main.java
@@ -1,5 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int i, j, k, linhasA, colunasA, linhasB, colunasB;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas da matriz A: ");
+        linhasA = in.nextInt();
+        System.out.print("Digite o número de colunas da matriz A: ");
+        colunasA = in.nextInt();
+
+        System.out.print("Digite o número de linhas da matriz B: ");
+        linhasB = in.nextInt();
+        System.out.print("Digite o número de colunas da matriz B: ");
+        colunasB = in.nextInt();
+
+        if (colunasA != linhasB) {
+            System.out.println("Multiplicação impossível: colunas de A devem ser iguais às linhas de B.");
+            return;
+        }
+
+        int[][] A = new int[linhasA][colunasA];
+        int[][] B = new int[linhasB][colunasB];
+        int[][] C = new int[linhasA][colunasB];
+
+        System.out.println("Digite os valores da matriz A:");
+        for (i = 0; i < linhasA; i++) {
+            for (j = 0; j < colunasA; j++) {
+                System.out.print("A[" + i + "][" + j + "] = ");
+                A[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println("Digite os valores da matriz B:");
+        for (i = 0; i < linhasB; i++) {
+            for (j = 0; j < colunasB; j++) {
+                System.out.print("B[" + i + "][" + j + "] = ");
+                B[i][j] = in.nextInt();
+            }
+        }
+
+        for (i = 0; i < linhasA; i++) {
+            for (j = 0; j < colunasB; j++) {
+                C[i][j] = 0;
+                for (k = 0; k < colunasA; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        System.out.println("Matriz produto:");
+        for (i = 0; i < linhasA; i++) {
+            for (j = 0; j < colunasB; j++) {
+                System.out.print(C[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
--- Listas de Exercícios/LP_Lista_05/Ex12_L5/src/Main.java
+++ Listas de Exercícios/LP_Lista_05/Ex12_L5/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[][] m = new int[3][3];
+        int i, j, determinante;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite os valores da matriz 3x3:");
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 3; j++) {
+                System.out.print("m[" + i + "][" + j + "] = ");
+                m[i][j] = in.nextInt();
+            }
+        }
+
+        determinante = m[0][0] * m[1][1] * m[2][2]
+                + m[0][1] * m[1][2] * m[2][0]
+                + m[0][2] * m[1][0] * m[2][1]
+                - m[0][2] * m[1][1] * m[2][0]
+                - m[0][0] * m[1][2] * m[2][1]
+                - m[0][1] * m[1][0] * m[2][2];
+
+        System.out.println("Determinante: " + determinante);
     }
 }
