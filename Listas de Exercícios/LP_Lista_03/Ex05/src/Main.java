15e83b6be227cfc14cd277f01dabb7e575eb8b10
LucasIanovski <ianovskilucas@gmail.com>
2025-06-15 18:20:18 -0300

    Revisão geral terceiro commit

--- Listas de Exercícios/LP_Lista_03/Ex05/src/Main.java
+++ Listas de Exercícios/LP_Lista_03/Ex05/src/Main.java
@@ -2,15 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        double n1, n2, media;  // Declaração das variáveis
+
         Scanner input = new Scanner(System.in);
-        double n1;
 
-        System.out.print("Digite um numero: ");
-        n1 = input.nextDouble();
+        System.out.println("Digite a primeira nota:");
+        n1 = input.nextDouble();  // Lê a primeira nota
 
-        while (n1 * 0); {
+        System.out.println("Digite a segunda nota:");
+        n2 = input.nextDouble();  // Lê a segunda nota
 
+        media = (n1 + n2) / 2;  // Calcula a média
 
-
-            }
+        // Verifica o conceito com base na média
+        if (media >= 8.5) {
+            System.out.println("Conceito A");
+        } else if (media >= 7) {
+            System.out.println("Conceito B");
+        } else if (media >= 5) {
+            System.out.println("Conceito C");
+        } else {
+            System.out.println("Conceito D");
         }
+    }
+}
